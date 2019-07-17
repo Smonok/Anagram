84ea042cb532e0a1e0869adff29860d0cceceba1
Nazarii Hubych <hubychnazarii@gmail.com>
2019-07-17 14:08:06 +0300

    Revert "Merge branch 'dev' into 'master'"
    This reverts merge request !4

--- anagrams/src/Main.java
+++
@@ -1,9 +0,0 @@
-import anagram.Anagram;
-public class Main {
-	
-	public static void main(String[] args) {
-		Anagram anagram = new Anagram();
-		String testStr = "d1c1ba hgf!e";
-		System.out.println(testStr + " => " + anagram.reverseEveryWord(testStr));	
-	}
-}
