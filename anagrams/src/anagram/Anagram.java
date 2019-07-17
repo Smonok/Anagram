55747572e291e93be2e1d65eacac33baf39f1c4d
Nazarii Hubych <hubychnazarii@gmail.com>
2019-07-18 01:29:55 +0300

    Revert "Removed unnecessary files"
    This reverts commit 4d9405750caf253fd9014690c18828453ba652e6.

---
+++ anagrams/src/anagram/Anagram.java
@@ -0,0 +1,33 @@
+package anagram;
+
+public class Anagram {
+	
+	 public String reverseEveryWord(String original){				
+		 	final String SPLIT_KEEPING_SPACES = "(?<= )|(?= )";
+		 	String[] splitStr = original.split(SPLIT_KEEPING_SPACES);		
+						
+			for(String splitted : splitStr) {
+				StringBuilder word = new StringBuilder(splitted);
+				char[] symbol = new char[word.length()];
+				int[] position = new int[word.length()];
+				int index = 0;
+				for(int j = 0; j < word.length(); j++) {
+					if(!Character.isLetter(word.charAt(j))) {
+						symbol[index] = word.charAt(j);
+						position[index] = j;
+						index++;
+					}						
+				}
+				
+				for(int i = index - 1; i >= 0; i--)
+					word.deleteCharAt(position[i]);
+				
+				word.reverse();
+				
+				for(int i = 0; i < index; i++)
+					word.insert(position[i], symbol[i]); 				
+			}
+						
+			return String.join("", splitStr);
+	 }	
+}
