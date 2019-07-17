8ad02dadf3bd530e15f0127f1dc10b0fe84220c6
Nazarii Hubych <hubychnazarii@gmail.com>
2019-07-18 01:43:25 +0300

    Revert "Cleaned the code and added gitignore"
    This reverts commit a33664a1048a204af60ad19ee8379817370dd52a.

--- Anagrams/src/Test.java
+++ Anagrams/src/Test.java
@@ -1,7 +1,8 @@
-import anagram.Anagram;
-public class Main {
-	
+
+public class Test {
+
 	public static void main(String[] args) {
+		
 		Anagram anagram = new Anagram();
 		String testStr = "d1c1ba hgf!e";
 		System.out.println(testStr + " => " + anagram.reverseEveryWord(testStr));	
