c459bf785d5eabc6c90f717c530ae715727ec84f
Nazarii Hubych <hubychnazarii@gmail.com>
2019-07-25 16:42:05 +0300

    test

---
+++ anagrams/src/anagram/Anagram.java
@@ -0,0 +1,48 @@
+package anagram;
+
+public class Anagram {
+    private final String WORDS_AND_SPACES = "(?<= )|(?= )";
+
+    public String reverseEveryWord(String original) {
+        String[] splitOriginal = original.split(WORDS_AND_SPACES);
+
+        for (int i = 0; i < splitOriginal.length; i++)
+            splitOriginal[i] = reverseOnlyLetters(splitOriginal[i]);
+
+        return String.join("", splitOriginal);
+    }
+
+    private String reverseOnlyLetters(String inputWord) {
+        StringBuilder word = new StringBuilder(inputWord);
+        char[] symbols = new char[word.length()];
+        int[] positions = new int[word.length()];
+        int number = findNotLetters(word, positions, symbols);
+
+        removeNotLetters(word, positions, number);
+        word.reverse();
+        insertNotLetters(word, positions, symbols, number);
+        return word.toString();
+    }
+
+    private void removeNotLetters(StringBuilder word, int[] positions, int count) {
+        for (int i = count - 1; i >= 0; i--)
+            word.deleteCharAt(positions[i]);
+    }
+
+    private void insertNotLetters(StringBuilder word, int[] positions, char[] symbols, int count) {
+        for (int i = 0; i < count; i++)
+            word.insert(positions[i], symbols[i]);
+    }
+
+    private int findNotLetters(StringBuilder word, int[] positions, char[] symbols) {
+        int index = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (!Character.isLetter(word.charAt(i))) {
+                symbols[index] = word.charAt(i);
+                positions[index] = i;
+                index++;
+            }
+        }
+        return index;
+    }
+}
